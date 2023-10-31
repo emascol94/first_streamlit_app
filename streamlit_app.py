2ebc309ad3bab4b849cca82941820248ddc3d660
emascol94 <35525766+emascol94@users.noreply.github.com>
2023-10-31 21:52:12 +0000

    Update streamlit_app.py
    Full message

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner') 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-boiled Free-ranged Egg')
