0c4ed1367d2238a7e59fd1cb862dc5642dff1c10
emascol94 <35525766+emascol94@users.noreply.github.com>
2023-11-05 00:18:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,12 +40,15 @@ try:
 except URLError as e:
     streamlit.error()
 
-
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+streamlit.stop()
+ 
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+             my_cur.execute("SELECT * from fruit_load_list")
+             return my_cur.fetchall() 
+                
+        streamlit.dataframe(my_data_rows) 
 
 
 def insert_row_snowflake(new_fruit):
